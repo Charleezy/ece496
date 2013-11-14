50c3830d56686dfde1b38c7ea53924c52489a38d
aguecida <d.agueci1@gmail.com>
2013-11-14 17:59:23 -0500

    Add popovers to display module details on homepage.

---
+++ ece496/ece496/Scripts/custom/homepage.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $("#teamDetails").popover({
+        html: true,
+        trigger: "hover",
+        content: function () {
+            return $('#teamDetails_msg').html();
+        }
+    });
+    $("#taskDetails").popover({
+        html: true,
+        trigger: "hover",
+        content: function () {
+            return $('#taskDetails_msg').html();
+        }
+    });
+    $("#calendarDetails").popover({
+        html: true,
+        trigger: "hover",
+        content: function () {
+            return $('#calendarDetails_msg').html();
+        }
+    });
+});
