1bc178bd1bfe25260eaa6924b475a40c51ebc404
aguecida <d.agueci1@gmail.com>
2014-01-08 20:10:54 -0500

    Import forms auth project.

---
+++ ece496/CustomMembershipEF/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomMembershipEF.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Protected()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult User(string username)
+        {
+            return View();
+        }
+    }
+}
