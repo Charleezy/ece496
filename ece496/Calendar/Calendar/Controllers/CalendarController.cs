13c71883b83a20a396c87c76c456bb9e0efa52d6
aguecida <d.agueci1@gmail.com>
2014-03-22 21:27:04 -0400

    Create new scheduler.

---
+++ ece496/Calendar/Calendar/Controllers/CalendarController.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using DHTMLX.Scheduler;
+using DHTMLX.Common;
+using DHTMLX.Scheduler.Data;
+using Calendar.Models;
+
+namespace Calendar.Controllers
+{
+    public class CalendarController : Controller
+    {
+        static int? uid;
+
+        public ActionResult Index(int? userid)
+        {
+            if (userid.HasValue)
+            {
+                uid = userid.Value;
+            }
+
+            var scheduler = new DHXScheduler(this);
+
+            scheduler.Skin = DHXScheduler.Skins.Glossy;
+            scheduler.InitialDate = DateTime.Now;
+
+            scheduler.Config.multi_day = true;//render multiday events
+
+            scheduler.LoadData = true;
+            scheduler.EnableDataprocessor = true;
+            scheduler.Data.DataProcessor.UpdateFieldsAfterSave = true;
+
+            scheduler.Config.first_hour = 9;
+
+            return View(scheduler);
+        }
+
+        public ContentResult Data()
+        {
+            var data = new SchedulerAjaxData();
+
+            if (uid != null)
+            {
+                data = new SchedulerAjaxData(
+                        new EventDataContext().Events.Where(x => x.user == uid)
+                    );
+            }
+
+            return (ContentResult)data;
+        }
+
+        public ContentResult Save(int? id, FormCollection actionValues)
+        {
+            var action = new DataAction(actionValues);
+            var changedEvent = (Event)DHXEventsHelper.Bind(typeof(Event), actionValues);
+            changedEvent.user = uid.Value;
+            var data = new EventDataContext();
+
+            switch (action.Type)
+            {
+                case DataActionTypes.Insert: // define here your Insert logic
+                    data.Events.InsertOnSubmit(changedEvent);
+                    break;
+                case DataActionTypes.Delete: // define here your Delete logic
+                    changedEvent = data.Events.SingleOrDefault(ev => ev.id == action.SourceId);
+                    data.Events.DeleteOnSubmit(changedEvent);
+                    break;
+                default:// "update" // define here your Update logic
+                    var eventToUpdate = data.Events.SingleOrDefault(ev => ev.id == action.SourceId);
+                    DHXEventsHelper.Update(eventToUpdate, changedEvent, new List<string>() { "id" });//update all properties, except for id
+                    break;
+            }
+            data.SubmitChanges();
+            action.TargetId = changedEvent.id;
+
+            var result = new AjaxSaveResponse(action);
+
+            return result;
+        }
+    }
+}
--- ece496/UnitTests/UnitTest1.cs
+++ ece496/UnitTests/UnitTest1.cs
@@ -10,9 +10,71 @@ namespace UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestTeamController()
         {
-            
+            // Initialize instance of Team Controller
+            CustomMembershipEF.Controllers.TeamController teamController = new CustomMembershipEF.Controllers.TeamController();
+
+            // Initialize CreateTeam parameters
+            string team1 = "Team 1";
+            string team2 = "Another really long team name 1234!";
+            string token1 = "ece496";
+            string token2 = "thistokendoesnotexist";
+
+            // Create a valid team
+            teamController.CreateTeam(team1, token1);
+            //Create a team with invalid parameters
+            teamController.CreateTeam(team2, token2);
+
+            // Initialize LeaveTeams parameters
+            string teamlist1 = "1,2,3,4";
+            string teamlist2 = "1,2,teamdoesnotexist,4";
+
+            // Leave teams with a valid team list
+            teamController.LeaveTeams(teamlist1);
+            // Leave teams with an invalid team list
+            teamController.LeaveTeams(teamlist2);
+
+            // Initailize SendInvite parameters
+            string sendTo1 = "daniele";
+            string sendTo2 = "not a real user";
+            string teamlist3 = "1,2,3,4";
+            string teamlist4 = "1,2,not a team number,4";
+
+            // Send a valid invitation
+            teamController.SendInvite(sendTo1, teamlist3);
+            // Send invitation to a user that doesn't exist
+            teamController.SendInvite(sendTo2, teamlist3);
+            // Send an invitation with an invalid team list
+            teamController.SendInvite(sendTo1, teamlist4);
+
+            // Initialize InviteResponse parameters
+            string answer1 = "ACCEPT";
+            string answer2 = "DECLINE";
+            string answer3 = "invalid response";
+            string inviteID1 = "1";
+            string inviteID2 = "invalid id";
+
+            // User accepts invitation
+            teamController.InviteResponse(inviteID1, answer1);
+            // User declines invitation
+            teamController.InviteResponse(inviteID1, answer2);
+            // User sends back an invalid response
+            teamController.InviteResponse(inviteID1, answer3);
+            // User sends response to invalid invite id
+            teamController.InviteResponse(inviteID2, answer1);
+
+            // Get invite count for current user
+            teamController.GetInviteCount();
+
+            // Get list on invites for current user
+            teamController.GetInvites();
+
+            // Get list of teams that the current user belongs to
+            teamController.GetTeamList();
+
+            Assert.AreEqual(0.01, 0.01, 0.001, "Account not debited correctly");
+
         }
     }
 }
