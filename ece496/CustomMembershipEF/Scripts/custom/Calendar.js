d19e2e5b64d595af5d89e10133b8d5a7f14583fb
aguecida <d.agueci1@gmail.com>
2014-03-26 18:41:16 -0400

    Add spinner to meeting scheduler results.

--- ece496/CustomMembershipEF/Scripts/custom/Calendar.js
+++ ece496/CustomMembershipEF/Scripts/custom/Calendar.js
@@ -1,4 +1,25 @@
-﻿$(function () {
+﻿var spinner;
+
+var spinoptions = {
+    lines: 10, // The number of lines to draw
+    length: 10, // The length of each line
+    width: 6, // The line thickness
+    radius: 10, // The radius of the inner circle
+    corners: 0.9, // Corner roundness (0..1)
+    rotate: 45, // The rotation offset
+    direction: 1, // 1: clockwise, -1: counterclockwise
+    color: '#000', // #rgb or #rrggbb or array of colors
+    speed: 1, // Rounds per second
+    trail: 67, // Afterglow percentage
+    shadow: false, // Whether to render a shadow
+    hwaccel: false, // Whether to use hardware acceleration
+    className: 'spinner', // The CSS class to assign to the spinner
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    top: '50px', // Top position relative to parent in px
+    left: 'auto' // Left position relative to parent in px
+};
+
+$(function () {
     $('#earliestDate').datetimepicker();
     $('#latestDate').datetimepicker();
 });
@@ -30,15 +51,19 @@ var search = function () {
         $('#search_err').show();
     }
     else {
+        // Clear previous result list
+        $('#results li').each(function () {
+            $(this).remove();
+        });
+
+        var targ = document.getElementById('results_body');
+        spinner = new Spinner(spinoptions).spin(targ);
+
         $.ajax({
             url: '/Calendar/SearchResults',
             data: { teamID: teamID, startDate: startDate, endDate: endDate, numResults: numResults, timeRequired: timeRequired },
             success: function (data) {
-                // Clear previous result list
-                $('#results li').each(function () {
-                    $(this).remove();
-                });
-
+                spinner.stop();
                 for (var i = 0; i < data.length; i++) {
                     $("#results").append('<li>' + data[i] + '</li>');
                 }
