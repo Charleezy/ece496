5da056d78b249e21e7d26f34ec999a3a2bdbca97
aguecida <d.agueci1@gmail.com>
2014-03-26 18:11:19 -0400

    Finish backend for meeting scheduler.

--- ece496/CustomMembershipEF/Controllers/CalendarController.cs
+++ ece496/CustomMembershipEF/Controllers/CalendarController.cs
@@ -6,14 +6,92 @@ using System.Web.Mvc;
 
 using CustomMembershipEF.Models;
 using CustomMembershipEF.Contexts;
+using System.Globalization;
 
 namespace CustomMembershipEF.Controllers
 {
     public class CalendarController : Controller
     {
-        public string SearchResults(int teamID, DateTime startDate, DateTime endDate, int numResults)
+        public DateTime RoundUp(DateTime dateTime)
         {
-            return null;
+            if (dateTime.Minute == 0)
+                return dateTime;
+            else
+                return new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour + 1, 0, 0, dateTime.Kind);
+        }
+
+        public DateTime RoundDown(DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour, 0, 0, dateTime.Kind);
+        }
+
+        public JsonResult SearchResults(int teamID, DateTime startDate, DateTime endDate, int numResults, int timeRequired)
+        {
+            int count = 0;
+            int flag = 0;
+            DateTime startDate_rounded, endDate_rounded, endDate_compare;
+            List<string> results = new List<string>();
+            List<TeamMember> teamMembers = new List<TeamMember>();
+
+            // Find team members of teamID
+            using (var teamsContext = new PM_Entities())
+            {
+                teamMembers = teamsContext.TeamMembers
+                                            .Where(x => x.FK_TeamID == teamID)
+                                            .ToList();
+            
+
+                // Round datetimes for easier calculations
+                startDate_rounded = RoundUp(startDate);
+                endDate_rounded = RoundDown(endDate);
+                endDate_compare = endDate_rounded.AddHours(-timeRequired);
+            
+                while (count < numResults && startDate_rounded < endDate_compare)
+                {
+                    flag = 0;
+                    DateTime endDate_temp = startDate_rounded.AddHours(timeRequired);
+
+                    // Check team member schedules and compare with current startDate_rounded -> endDate_temp
+                    foreach (var member in teamMembers)
+                    {
+                        // Check if there is an event where start_date > startDate_rounded and start_date < endDate_temp
+                        // or if there is an event where end_date > startDate_rounded and end_date < endDate_temp
+                        // If either is true, this time slot will not work for the team
+                        int event_count = teamsContext.Events
+                                            .Where(x => x.type == "event" && 
+                                                        x.user == member.FK_UserID &&
+                                                       (x.start_date > startDate_rounded && x.start_date < endDate_temp ||
+                                                        x.end_date > startDate_rounded && x.end_date < endDate_temp)
+                                                  )
+                                            .Count();
+                        
+                        if (event_count != 0)
+                        {
+                            flag = 1;
+                            break;
+                        }
+                    }
+
+                    // If no team member was busy during this time period, add result and increment count
+                    if (flag == 0)
+                    {
+                        // Format string so it is in readable date format
+                        string newResult = startDate_rounded.DayOfWeek.ToString() + " " + 
+                                            startDate_rounded.ToString("MMM", CultureInfo.InvariantCulture) + " " +
+                                            startDate_rounded.Day.ToString() + ", " +
+                                            startDate_rounded.Hour.ToString() + startDate_rounded.ToString("tt") + " - " +
+                                            (startDate_rounded.Hour + timeRequired).ToString() + startDate_rounded.ToString("tt");
+
+                        results.Add(newResult);
+                        count++;
+                    }
+
+                    startDate_rounded = startDate_rounded.AddHours(1);
+                }
+
+            }
+
+            return Json(results, JsonRequestBehavior.AllowGet);
         }
     }
 }
--- ece496/CustomMembershipEF/Scripts/custom/Calendar.js
+++ ece496/CustomMembershipEF/Scripts/custom/Calendar.js
@@ -11,6 +11,12 @@ $(document).ready(function () {
     $('#latestDate').focus(function () {
         $('#search_err').hide();
     });
+
+    $('#clear_results').click(function () {
+        $('#results li').each(function () {
+            $(this).remove();
+        });
+    });
 });
 
 var search = function () {
@@ -18,16 +24,24 @@ var search = function () {
     var startDate = $('#earliestDate').val();
     var endDate = $('#latestDate').val();
     var numResults = $('#numResults').val();
-
+    var timeRequired = $('#timeRequired').val();
+    
     if (startDate == "" || startDate == null || endDate == "" || endDate == null) {
         $('#search_err').show();
     }
     else {
         $.ajax({
             url: '/Calendar/SearchResults',
-            data: { teamID: teamID, startDate: startDate, endDate: endDate, numResults: numResults },
-            success: function (msg) {
-                alert("success");
+            data: { teamID: teamID, startDate: startDate, endDate: endDate, numResults: numResults, timeRequired: timeRequired },
+            success: function (data) {
+                // Clear previous result list
+                $('#results li').each(function () {
+                    $(this).remove();
+                });
+
+                for (var i = 0; i < data.length; i++) {
+                    $("#results").append('<li>' + data[i] + '</li>');
+                }
             }
         });
     }
